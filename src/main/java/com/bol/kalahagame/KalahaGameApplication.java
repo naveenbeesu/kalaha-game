94d9015d7cc849ca5664d18bb6f42d3b1354281a
nbeesu <naveen.beesu1993@gmail.com>
2023-08-28 23:16:22 +0200

    Adds basic structure with dependencies

---
+++ src/main/java/com/bol/kalahagame/KalahaGameApplication.java
@@ -0,0 +1,13 @@
+package com.bol.kalahagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalahaGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalahaGameApplication.class, args);
+	}
+
+}
